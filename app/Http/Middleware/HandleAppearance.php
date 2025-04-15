235e52c303725eb329b0b2ac2e7c9ab9b0b03bb1
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-15 22:03:40 +0100

    4-Handle an incoming request.

--- app/Http/Middleware/HandleAppearance.php
+++ app/Http/Middleware/HandleAppearance.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class HandleAppearance
 {
     /**
-     * Handle an incoming request.
+     * 4-Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
